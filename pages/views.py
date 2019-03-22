cf4ddbca230ffb2c604ea9dd372ce824eeac9ed1
aayushraj07 <aayush.97raj@gmail.com>
2019-03-22 20:00:25 +0530

    Pages page added

--- pages/views.py
+++ pages/views.py
@@ -7,3 +7,15 @@ from django.shortcuts import render
 
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h1>Contact Page</h1>")
+
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h1>About Page</h1>")
+
+
+def social_view(*args, **kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
